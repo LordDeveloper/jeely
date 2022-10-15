12ceee430aace710ffdfbb2cce334e50cf202e06
lorddeveloper <losrkzpp@hi2.in>
2022-10-16 01:21:57 +0330

    Added array accessing feature to lazy updates

--- src/Jeely/LazyUpdates.php
+++ src/Jeely/LazyUpdates.php
@@ -4,7 +4,7 @@ namespace Jeely;
 
 use LazyJsonMapper\LazyJsonMapper;
 
-class LazyUpdates extends LazyJsonMapper
+class LazyUpdates extends LazyJsonMapper implements \ArrayAccess
 {
     protected static Telegram $api;
 
@@ -18,5 +18,25 @@ class LazyUpdates extends LazyJsonMapper
     public function isOk(): bool
     {
         return true;
+    }
+
+    public function offsetExists(mixed $offset): bool
+    {
+        return isset($this->{$offset});
+    }
+
+    public function offsetGet(mixed $offset): mixed
+    {
+        return $this->{$offset};
+    }
+
+    public function offsetSet(mixed $offset, mixed $value)
+    {
+        $this->{$offset} = $value;
+    }
+
+    public function offsetUnset(mixed $offset)
+    {
+        unset($this->{$offset});
     }
 }
